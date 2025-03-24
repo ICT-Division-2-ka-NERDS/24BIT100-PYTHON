b9fd66bb3e4bb5b7a381903077ad18e470255214
RonitKundnani <ronitkundnani08@gmail.com>
2025-03-24 14:12:10 +0530

    Initial commit

---
+++ 9pdf/2/1.py
@@ -0,0 +1,54 @@
+# def q1():
+#     def isPrime(n:int):
+#         if n==2: return True
+#         for i in range(2,(n//2)+1):
+#             if n%i==0:
+#                 return False
+#             else:
+#                 pass
+#         return True
+#     l=[]
+#     def primeFactors(n:int,i=2):
+#         if i==n:
+#             print(l)
+#             return
+#         if n%i==0:
+#             if isPrime(i):
+#                 l.append(i)
+#             primeFactors(n,i=i+1)
+#         else:
+#             primeFactors(n,i=i+1)
+#     primeFactors(10)
+# q1()
+
+
+
+
+
+
+def q1():
+    def isPrime(n: int) -> bool:
+        if n < 2:
+            return False
+        if n == 2:
+            return True
+        for i in range(2, int(n**0.5) + 1):  # Check only up to √n
+            if n % i == 0:
+                return False
+        return True
+
+    l = []  # List to store prime factors
+
+    def primeFactors(n: int, i=2):
+        if n <= 1:  # Base case: when n is fully reduced
+            print(l)
+            return
+        while n % i == 0:
+            l.append(i)
+            n //= i  # Reduce n
+        primeFactors(n, i + 1)  # Move to the next factor
+
+    num = int(input("Enter a number: "))  # Allow user input
+    primeFactors(num)
+
+q1()
